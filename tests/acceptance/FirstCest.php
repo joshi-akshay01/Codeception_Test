7b64e96a059ba55201a5c0dc1e4e4a5d6d2d9e96
Akshay Joshi <akshay.joshi@synbuf.local>
2019-01-24 16:29:30 +0530

    reports and simple pro

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -18,9 +18,7 @@ class FirstCest
 
    public function Check_Authentication_Atlantic_Broadband(AcceptanceTester $I)
 	{
-		global $Client,$Username,$Password;
-		$ini_array = (parse_ini_file("data.ini",true, INI_SCANNER_RAW));
-		foreach ($ini_array as $data) {
+		{
 			$I->amOnPage('/');
 			$I->click('html body div#body-container form input');
 			$I->selectOption('//*[@id="dropdownlist"]','Atlantic Broadband (auth.atlanticbb.net)');
@@ -30,7 +28,6 @@ class FirstCest
 			$I->click('//*[@id="login"]'); 
 			$I->wait(10);
 			$I->seeInTitle('SAML 2.0 SP Demo Example');
-			$I->click('/html/body/div[2]/p[4]/a');
 		}
 	}		
 
